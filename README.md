647f9cab5b1502d682d183d03ec333f6a940b08e
AugustoSavi <guto_savi@outlook.com>
2021-01-08 22:04:50 -0300

    version 0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ConvertJavaClassToAsvc
+<h1 align="center">
+    Convert Java Class To Asvc
+</h1>
+
+## :information_source:  Atenção
+Isso aqui foi feito da forma mais rapida possivel e não seguiu nenhum padrão
+
+## :information_source: Como usar
+
+Copie a classe inteira da sua IDEs e clique em converter
+
+![gifConvertClasseJava](https://user-images.githubusercontent.com/32443720/104078520-491ac900-51fd-11eb-9dec-3b5f7d68f45b.gif)
---
+++ src/main.js
@@ -0,0 +1,81 @@
+function convert(){
+    let classString = document.getElementById("textAreaInput").value;
+    let outputClassString = document.getElementById("textAreaResult");
+    // Pega a linha completa que inicia com "private"
+    const regex = /private(...*);\n/gm;
+    let variaveisBeforeSplit = classString.match(regex)
+    let variaveisAfterSplit=[];
+    let jsonAfterwork = "";
+
+    //Cria um array de array do tipo e nome da variavel 
+    variaveisBeforeSplit.forEach(element => {
+        variaveisAfterSplit.push(element.split(" "))
+    });
+
+    //Cria o objeto e o json
+   variaveisAfterSplit.forEach(element => {
+    objeto = constroiObjeto(element[2].trim(),element[1])
+    jsonAfterwork = jsonAfterwork.concat(JSON.stringify(objeto,null,4),",")
+   });
+
+   //insere no result
+   outputClassString.value = jsonAfterwork;
+}
+
+//Retorna os tipo da maneira em que é necessario
+function tratamentoType(nameTipoVariavel){
+
+    switch(true) {
+        case nameTipoVariavel.includes("Long"):
+        return "long";
+        
+        case nameTipoVariavel.includes("String"):
+        return "string";
+        
+        case nameTipoVariavel.includes("Integer"):
+        return "int";
+        
+        case nameTipoVariavel.includes("Boolean"):
+        return "boolean";
+        
+        case nameTipoVariavel.includes("LocalDate"):
+        return "long";
+        
+        case nameTipoVariavel.includes("BigDecimal"):
+        return "bigdecimal";
+        
+        case nameTipoVariavel.includes("List"):
+        return {
+            type: "array",
+            items: {
+            type: "record",
+            name: "NomeEvent",
+            namespace:"com.betha.saude.avro.CAMINHOEVENTO",
+            fields: []
+            }
+        }
+
+        default:
+        return "long";
+        
+    }
+}
+
+//Constroi o Objeto de forma que se for um List ele monta diferente
+function constroiObjeto(name,tipo){
+
+    name = name.replace(";","");
+
+    if(tipo.includes("List")){
+        return { 
+            name,
+            type: tratamentoType(tipo)
+        }
+    }
+
+    return{
+        name,
+        default: null,
+        type: [null, tratamentoType(tipo) ]
+    }
+}
